b530884670e621d632b08705e56dbfcf31b46754
Emilio Marcos <emch_98@hotmail.com>
2021-04-12 01:38:30 -0500

    Initial Commit

---
+++ Shared/Learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GHApp.swift
+//  Shared
+//
+//  Created by Emilio Marcos Chapa on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
